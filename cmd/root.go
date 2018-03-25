7638310df096844814adc63775225d203b1209c2
Joel Ferrier <joel@ferrier.io>
2018-03-25 13:01:38 -0700

    Initial Commit

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "margarita-mixer",
+	Short: "margarita-mixer is a LiME module build tool.",
+	Long: `margarita-mixer is LiME module build tool which
+uses docker to compile LiME kernel modules and creates
+a searchable repository of kernel modules.`,
+	Hidden: true,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
